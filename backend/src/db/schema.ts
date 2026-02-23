9d189681e6f6b7d9f2eab9f79b813c34981c909a
Zeeshan <sk.zeeshan1992@gmail.comn>
2026-02-22 20:46:34 -0500

    update users table to set updatedAt field to current date on update

--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -7,7 +7,10 @@ export const users = pgTable("users", {
   name: text("name"),
   imageUrl: text("image_url"),
   createdAt: timestamp("create_at", { mode: "date" }).notNull().defaultNow(),
-  updatedAt: timestamp("updated_at", { mode: "date" }).notNull().defaultNow(),
+  updatedAt: timestamp("updated_at", { mode: "date" })
+    .notNull()
+    .defaultNow()
+    .$onUpdate(() => new Date()),
 })
 
 export const products = pgTable("products", {
