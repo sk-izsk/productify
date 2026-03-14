1dbf205cc230656081b061eec9494dd22423b41e
Zeeshan <sk.zeeshan1992@gmail.comn>
2026-03-14 16:42:13 -0400

    update edit page

--- frontend/src/Screen/EditProductScreen.tsx
+++ frontend/src/Screen/EditProductScreen.tsx
@@ -1,7 +1,42 @@
+import { useAuth } from "@clerk/react"
 import React from "react"
+import { useNavigate, useParams } from "react-router"
+import { EditProductForm } from "../components/EditProductForm"
+import { useGetProduct } from "../hooks/web/useGetProduct"
+import { useUpdateProduct } from "../hooks/web/useUpdateProduct"
+import type { Product } from "../types"
+import { Screen } from "./Screen"
+
+type EditProductParams = {
+  id?: string
+}
 
 const EditProductScreen: React.FC = () => {
-  return <div></div>
+  const { id } = useParams<EditProductParams>()
+  const { userId } = useAuth()
+  const navigate = useNavigate()
+  const { data: product, isLoading, isError } = useGetProduct(id)
+  const updateProduct = useUpdateProduct()
+  return (
+    <Screen
+      isError={isError || !product || product.userId !== userId}
+      isLoading={isLoading}
+    >
+      <EditProductForm
+        product={product!}
+        isPending={updateProduct.isPending}
+        isError={updateProduct.isError}
+        onSubmit={(formData: Partial<Product>) => {
+          updateProduct.mutate(
+            { productId: id as string, formData },
+            {
+              onSuccess: () => navigate(`/product/${id}`),
+            },
+          )
+        }}
+      />
+    </Screen>
+  )
 }
 
 export default EditProductScreen
---
+++ frontend/src/hooks/web/useUpdateProduct.ts
@@ -0,0 +1,18 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+import { updateProduct } from "../../api/api"
+
+export const useUpdateProduct = () => {
+  const queryClient = useQueryClient()
+  return useMutation({
+    mutationKey: ["updateProduct"],
+    mutationFn: updateProduct,
+    onSuccess: (_, variables) => {
+      queryClient.invalidateQueries({
+        queryKey: ["product", variables.productId],
+      })
+      queryClient.invalidateQueries({
+        queryKey: ["myProducts"],
+      })
+    },
+  })
+}
